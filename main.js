46d71bcb271a0a84567a212f4ad87d64febfc7a9
Dalton Hart <DaltonVaren@gmail.com>
2019-02-06 19:34:00 -0800

    added additonal files

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-const { app, BrowserWindow } = require('electron')
+const {
+  app,
+  BrowserWindow
+} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
 
   // and load the index.html of the app.
   win.loadFile('index.html')
