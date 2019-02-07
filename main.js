333081613af84e17fec355650aa7db8aba1c08e7
Dalton Hart <DaltonVaren@gmail.com>
2019-02-06 22:17:09 -0800

    added logic for final build

--- main.js
+++ main.js
@@ -16,7 +16,6 @@ function createWindow() {
 
   // and load the index.html of the app.
   win.loadFile('index.html')
-  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
